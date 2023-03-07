87dead7820bef7ed69015fc1bebb620c9f2567c4
Jorge Antonio Macias Zambrano <jorgemacias7905@alumnos.udg.mx>
2023-03-07 12:56:12 -0600

    Add: reiniciar proyecto
    	He tenido problemas con los importes de css
    	por lo que tuve que reiniciar el desarrollo del frontend F

--- frontend/src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Footer = () => {
-  return (
-    <div>
-      <h2>Footer funciona</h2>
-    </div>
-  )
-}
--- frontend/src/components/navbar/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar funciona!</h1>
-    </div>
-  )
-}
