40cfc0aff260937befbc71348e907ce185c8c566
Jorge Antonio Macias Zambrano <jorgemacias7905@alumnos.udg.mx>
2023-03-07 16:53:37 -0600

    Add: constantes para el index

---
+++ frontend/src/constants/HomePage.ts
@@ -0,0 +1,5 @@
+export const title = "The Cookie Trail";
+export const description = "Plataforma de micro inversiones, que permita recaudar e invertir fondos para proyectos utilizando criptomonedas.";
+
+export const button_text_invest = "Invest";
+export const button_text_create = "Create";
