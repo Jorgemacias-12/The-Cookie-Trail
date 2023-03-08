47012d441b9c5e99ac9c75fa3f94693011dcce23
Jorgemacias-12 <jorgemacias7905@alumnos.udg.mx>
2023-03-08 07:19:38 -0600

    Add: terminar la estructura del layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { ReactNode } from 'react';
 
-import styles from '../../styles/Layout.module.css';
+import styles from '@/styles/Layout.module.css';
+import { Navbar } from '../navbar/Navbar';
+import { Footer } from '../footer/Footer';
 
-export const Layout = () => {
+type LayoutProps = {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={""}>
-      
+    <div className={styles.appContainer}>
+      <Navbar />
+      <main id="content" className={styles.content}>
+        {children}
+      </main>
+      <Footer />
     </div>
   )
 }
