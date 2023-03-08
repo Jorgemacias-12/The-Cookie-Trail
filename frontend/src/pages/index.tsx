648722be43ac4bb378cc6f5054855c12ac2bbc28
Jorgemacias-12 <jorgemacias7905@alumnos.udg.mx>
2023-03-08 06:57:32 -0600

    Add: agregar contenido a ruta base
    	No he terminado de subir todo el contenido

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,26 @@
+import styles from '@/styles/Home.module.css'
+
 export default function Home() {
   return (
     <>
-     
+      <article className={styles.wrapper}>
+        <section className={styles.container}>
+
+          <h2 className={styles.title}>
+            The Cookie Trail
+          </h2>
+
+          <p className={styles.description}>
+            Plataforma de micro inversiones, que permita recaudar e invertir fondos para proyectos.
+          </p>
+
+        </section>
+
+        <section className={styles.callToAction}>
+          <button type="button" className={styles.button}>Invertir</button>
+          <button type="button" className={styles.button}>Crear</button>
+        </section>
+      </article>
     </>
   )
 }
