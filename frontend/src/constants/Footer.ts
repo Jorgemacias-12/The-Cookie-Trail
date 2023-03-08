45dd99d94f8a48a4cdcd6ead97ccd8dfb41fdedd
Jorgemacias-12 <jorgemacias7905@alumnos.udg.mx>
2023-03-08 07:08:51 -0600

    Add: constantes para el footer y menu

---
+++ frontend/src/constants/Footer.ts
@@ -0,0 +1,14 @@
+export const aboutUs = "Acerca de"
+
+export const aboutUsSection = [
+  "Equipo",
+  "Tecnología",
+  "Plataforma"
+]
+
+export const notice = "Aviso";
+
+export const  noticeSection = [
+  "Legal",
+  "Privacidad",
+]
---
+++ frontend/src/constants/Menu.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    name: "Inicio",
+    link: "/"
+  },
+  {
+    name: "Acerca de",
+    link: "/acerca-de",
+  },
+  {
+    name: "Conocenos",
+    link: "/conocenos",
+  },
+  {
+    name: "Contacto",
+    link: "/contacto",
+  },
+]
